312bf0c0c3f684e8570333513e934afa329febac
Marco Contreras <mcontreras42@outlook.com>
2024-10-31 01:17:44 -0300

    completando archivo de consultando el dom con getelement y queryselector

--- 3-ConsultandoElDomConGetElementByIdYquerySelector.js
+++ 3-ConsultandoElDomConGetElementByIdYquerySelector.js
@@ -33,3 +33,19 @@ Con document.querySelector("p") este nos trae el primer elemento que contenga es
 
 Para traer todos los elementos que contenga una clase debemos usar el siguiente metodo. 
 document.getElementByClassName("Nombre_clase") 
+Esto nos trae una coleccion de los elementos que tengan el nombre de la clase la cual estamos buscando. 
+Ademas se pueden hacer diferentes acciones como agregar, cambiar o modificar incluso
+
+Con document.getElementsByTagName("p") con este metodo nos trae todos los elemetos que contenga la etiqueta 
+parrafo como una coleccion y una vez estemos desplegando cada elemento en la crossOriginIsolated, podemos
+acceder al nodo o objeto y podemos  indagar que trae por ejemplo el metodo innerText o innerHTML para poder 
+manipular ese texto del parrafo. 
+
+Con document.querySelectorAll("nombre_clase") de forma implicita nos dice que nos puede traer todos los elementos que contenga 
+la clase. como esto funciona para clases, podemos iniciar la busqueda con un punto (.) y luego el nombre 
+de la clase, esto nso entrega una lista de los nodos, dentro de los nodos podemos encontrar elementos HTML, tambien texto o nodos 
+que se van generando al momento de estar trabajando en el DOM. 
+Al abrir la lista tenemos acceso al objeto de ese nodo para asi generar la manipulacion que estamos buscando. 
+
+Respecto a la velocidad que queramos tener para hacer la busqueda es preferible usar getElement o getElements que querySelector ya que 
+estos son mas nuevos.  
