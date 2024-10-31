db26137895c35c509fa59b8ba6a77deb15549db5
Marco Contreras <mcontreras42@outlook.com>
2024-10-31 11:38:26 -0300

    terminando archivo de get element y query selector

--- 3-ConsultandoElDomConGetElementByIdYquerySelector.js
+++ 3-ConsultandoElDomConGetElementByIdYquerySelector.js
@@ -10,42 +10,42 @@
 
 // ¿Como seleccionar elementos?
 
- Los elementos se pueden seleccionar por ejemplo con su ID,
- su clase, por las etiquetas o por atributos que contenga una 
- etiqueta en particular.
+//  Los elementos se pueden seleccionar por ejemplo con su ID,
+//  su clase, por las etiquetas o por atributos que contenga una 
+//  etiqueta en particular.
 
- Metodo de como seleccionar con document. 
+//  Metodo de como seleccionar con document. 
  
- document.getElementByid("nombre_del_ID") = con esta seleccion seleccionamos los elementos que sean unicos por ejemplo un ID 
-es importante que un html no se puede repetir ningun elemento pro ejemplo con los id, que deben ser unicos 
-a diferencia que las clases que pueden compartir los elementos.
-Con este metodo nos trae el elemento que contenga ese nombre del id. 
-Un uso que le podemos dar es que podemos guardar en una variable. 
+//  document.getElementByid("nombre_del_ID") = con esta seleccion seleccionamos los elementos que sean unicos por ejemplo un ID 
+// es importante que un html no se puede repetir ningun elemento pro ejemplo con los id, que deben ser unicos 
+// a diferencia que las clases que pueden compartir los elementos.
+// Con este metodo nos trae el elemento que contenga ese nombre del id. 
+// Un uso que le podemos dar es que podemos guardar en una variable. 
 
-Otra forma de poder seleccionar es con un metodo nuevo que se llama
-document.querySelector() con esto podemos sleccionar ID o algun atributo en 
-especifico o etiqueta 
+// Otra forma de poder seleccionar es con un metodo nuevo que se llama
+// document.querySelector() con esto podemos sleccionar ID o algun atributo en 
+// especifico o etiqueta 
 
-Ejemplo. document.querySelector("#+Nombre_id") y esto nos trae el elemento que contenga 
-dicho id. 
+// Ejemplo. document.querySelector("#+Nombre_id") y esto nos trae el elemento que contenga 
+// dicho id. 
 
-Con document.querySelector("p") este nos trae el primer elemento que contenga esa etiqueta. 
+// Con document.querySelector("p") este nos trae el primer elemento que contenga esa etiqueta. 
 
-Para traer todos los elementos que contenga una clase debemos usar el siguiente metodo. 
-document.getElementByClassName("Nombre_clase") 
-Esto nos trae una coleccion de los elementos que tengan el nombre de la clase la cual estamos buscando. 
-Ademas se pueden hacer diferentes acciones como agregar, cambiar o modificar incluso
+// Para traer todos los elementos que contenga una clase debemos usar el siguiente metodo. 
+// document.getElementByClassName("Nombre_clase") 
+// Esto nos trae una coleccion de los elementos que tengan el nombre de la clase la cual estamos buscando. 
+// Ademas se pueden hacer diferentes acciones como agregar, cambiar o modificar incluso
 
-Con document.getElementsByTagName("p") con este metodo nos trae todos los elemetos que contenga la etiqueta 
-parrafo como una coleccion y una vez estemos desplegando cada elemento en la crossOriginIsolated, podemos
-acceder al nodo o objeto y podemos  indagar que trae por ejemplo el metodo innerText o innerHTML para poder 
-manipular ese texto del parrafo. 
+// Con document.getElementsByTagName("p") con este metodo nos trae todos los elemetos que contenga la etiqueta 
+// parrafo como una coleccion y una vez estemos desplegando cada elemento en la crossOriginIsolated, podemos
+// acceder al nodo o objeto y podemos  indagar que trae por ejemplo el metodo innerText o innerHTML para poder 
+// manipular ese texto del parrafo. 
 
-Con document.querySelectorAll("nombre_clase") de forma implicita nos dice que nos puede traer todos los elementos que contenga 
-la clase. como esto funciona para clases, podemos iniciar la busqueda con un punto (.) y luego el nombre 
-de la clase, esto nso entrega una lista de los nodos, dentro de los nodos podemos encontrar elementos HTML, tambien texto o nodos 
-que se van generando al momento de estar trabajando en el DOM. 
-Al abrir la lista tenemos acceso al objeto de ese nodo para asi generar la manipulacion que estamos buscando. 
+// Con document.querySelectorAll("nombre_clase") de forma implicita nos dice que nos puede traer todos los elementos que contenga 
+// la clase. como esto funciona para clases, podemos iniciar la busqueda con un punto (.) y luego el nombre 
+// de la clase, esto nso entrega una lista de los nodos, dentro de los nodos podemos encontrar elementos HTML, tambien texto o nodos 
+// que se van generando al momento de estar trabajando en el DOM. 
+// Al abrir la lista tenemos acceso al objeto de ese nodo para asi generar la manipulacion que estamos buscando. 
 
-Respecto a la velocidad que queramos tener para hacer la busqueda es preferible usar getElement o getElements que querySelector ya que 
-estos son mas nuevos.  
+// Respecto a la velocidad que queramos tener para hacer la busqueda es preferible usar getElement o getElements que querySelector ya que 
+// estos son mas nuevos.  
